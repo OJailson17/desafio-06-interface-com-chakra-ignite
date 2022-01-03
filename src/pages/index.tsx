687c6f379ec79f8244cf6bded2587980f9399adc
OJailson17 <JaylsonO17@gmail.com>
2022-01-03 05:50:12 -0300

    add banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,35 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <Heading>Hello World</Heading>;
+  return (
+    <Flex
+      w='100%'
+      h={335}
+      bgImage="url('/assets/background.png')"
+      backgroundPosition='center'
+      backgroundRepeat='no-repeat'
+      backgroundSize='100%'
+      position='relative'
+      justify='space-around'
+      align='center'
+    >
+      <Box color='gray.50'>
+        <Heading fontWeight='medium'>
+          5 Continentes, <br /> infinitas possibilidades.
+        </Heading>
+        <Text fontSize='20px' mt='20px' fontWeight='normal'>
+          Chegou a hora de tirar do papel a viagem que você <br /> sempre
+          sonhou.{' '}
+        </Text>
+      </Box>
+
+      <Box mt={24}>
+        <Image src='/assets/airplane.svg' alt='airplane' />
+      </Box>
+    </Flex>
+  );
 };
 
 export default Home;
