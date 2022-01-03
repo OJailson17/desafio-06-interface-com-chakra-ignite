90edc555091f4959f4dfb4a041df14bea2e04ee6
OJailson17 <JaylsonO17@gmail.com>
2022-01-03 05:55:13 -0300

    make banner a component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+
+export const Banner = () => {
+  return (
+    <Flex
+      w='100%'
+      h={335}
+      bgImage="url('/assets/background.png')"
+      backgroundPosition='center'
+      backgroundRepeat='no-repeat'
+      backgroundSize='100%'
+      position='relative'
+      justify='space-around'
+      align='center'
+    >
+      <Box color='gray.50'>
+        <Heading fontWeight='medium'>
+          5 Continentes, <br /> infinitas possibilidades.
+        </Heading>
+        <Text fontSize='20px' mt='20px' fontWeight='normal'>
+          Chegou a hora de tirar do papel a viagem que você <br /> sempre
+          sonhou.{' '}
+        </Text>
+      </Box>
+
+      <Box mt={24}>
+        <Image src='/assets/airplane.svg' alt='airplane' />
+      </Box>
+    </Flex>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,10 @@
 import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Banner } from '../components/Banner';
 
 const Home: NextPage = () => {
-  return (
-    <Flex
-      w='100%'
-      h={335}
-      bgImage="url('/assets/background.png')"
-      backgroundPosition='center'
-      backgroundRepeat='no-repeat'
-      backgroundSize='100%'
-      position='relative'
-      justify='space-around'
-      align='center'
-    >
-      <Box color='gray.50'>
-        <Heading fontWeight='medium'>
-          5 Continentes, <br /> infinitas possibilidades.
-        </Heading>
-        <Text fontSize='20px' mt='20px' fontWeight='normal'>
-          Chegou a hora de tirar do papel a viagem que você <br /> sempre
-          sonhou.{' '}
-        </Text>
-      </Box>
-
-      <Box mt={24}>
-        <Image src='/assets/airplane.svg' alt='airplane' />
-      </Box>
-    </Flex>
-  );
+  return <Banner />;
 };
 
 export default Home;
