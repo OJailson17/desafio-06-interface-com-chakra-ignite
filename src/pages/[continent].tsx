68b20b96d93e7a746b8139b3d1c217b3e4cc4d80
OJailson17 <JaylsonO17@gmail.com>
2022-01-07 11:24:45 -0300

    add continent info

--- src/pages/[continent].tsx
+++ src/pages/[continent].tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, HStack, Text } from '@chakra-ui/react';
 
 export default function Continent() {
   return (
@@ -12,9 +12,61 @@ export default function Continent() {
         backgroundSize='100vw'
         position='relative'
       >
-        <Heading color='gray.50' position='absolute' bottom={16} left={36}>
+        <Heading
+          color='gray.50'
+          fontWeight='semibold'
+          fontSize='48px'
+          position='absolute'
+          bottom={16}
+          left={36}
+        >
           Europa
         </Heading>
+      </Flex>
+
+      <Flex
+        align='center'
+        w='80%'
+        justify='space-between'
+        m='0 auto'
+        mt='80px'
+        mb='80px'
+      >
+        <Box w='50%' mr='70px'>
+          <Text fontSize='24px' textAlign='justify'>
+            A Europa é, por convenção, um dos seis continentes do mundo.
+            Compreendendo a península ocidental da Eurásia, a Europa geralmente
+            divide-se da Ásia a leste pela divisória de águas dos montes Urais,
+            o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+          </Text>
+        </Box>
+
+        <HStack spacing={10}>
+          <Box>
+            <Text color='yellow.400' fontSize='48px' fontWeight='semibold'>
+              50
+            </Text>
+            <Text fontSize='24px' fontWeight='semibold'>
+              países
+            </Text>
+          </Box>
+          <Box>
+            <Text color='yellow.400' fontSize='48px' fontWeight='semibold'>
+              60
+            </Text>
+            <Text fontSize='24px' fontWeight='semibold'>
+              línguas
+            </Text>
+          </Box>
+          <Box>
+            <Text color='yellow.400' fontSize='48px' fontWeight='semibold'>
+              27
+            </Text>
+            <Text fontSize='24px' fontWeight='semibold'>
+              cidades +100
+            </Text>
+          </Box>
+        </HStack>
       </Flex>
     </>
   );
